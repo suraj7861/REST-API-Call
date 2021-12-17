dfa54e7052b807c0c049e685f31a534a0532796b
unknown <khomanesuraj8@gmail.com>
2021-12-17 18:09:11 +0530

    Using post method Rest API call to display first and last name

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -19,7 +19,7 @@ import com.example.demo.model.User;
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
-	
+
 	@RequestMapping(value = { "", "/", "/home" })
 	public String sayHello() {
 		return "Hello From BridgeLabz";
@@ -34,26 +34,27 @@ public class HelloRestController {
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name + " From Bridgelabz";
 	}
-	
+
 	/**
 	 * @GetMapping: mapping HTTP GET requests onto specific handler methods
-	 * @PathVariable: indicates that a method parameter should be bound to a URI template variable.
+	 * @PathVariable: indicates that a method parameter should be bound to a URI
+	 *                template variable.
 	 * @param name
 	 * @return
 	 */
-    @GetMapping("/param/{name}")
-    public String sayHelloPara(@PathVariable String name) {
-        return "Hello " + name + " From Bridgelabz";
-    }
+	@GetMapping("/param/{name}")
+	public String sayHelloPara(@PathVariable String name) {
+		return "Hello " + name + " From Bridgelabz";
+	}
 
-    /**
-     * @PostMapping:handle the HTTP POST requests matched with given URI expression
-     * @RequestBody: 
-     * @return
-     */
-    @PostMapping("/post")
-    public String sayHello(@RequestBody User user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
-    }
-   
+	/**
+	 * @PostMapping:handle the HTTP POST requests matched with given URI expression
+	 * @RequestBody:
+	 * @return
+	 */
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
+	}
+
 }

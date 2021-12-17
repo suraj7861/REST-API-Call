20af5c9984275dcdeea05d290d5d4ba6f693a819
unknown <khomanesuraj8@gmail.com>
2021-12-17 11:15:16 +0530

    pass name as query parameter and display message

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,20 +1,33 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
  * @RestController:descibes the rest controller class.
- * @RequestMapping:describes the URL.
+ * @RequestMapping:describe the URL.
  *
  */
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
+	
 	@RequestMapping(value = { "", "/", "/home" })
 	public String sayHello() {
 		return "Hello From BridgeLabz";
+	}
+
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + " From Bridgelabz";
 	}
 
 }

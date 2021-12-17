9dba2761e56d840839fbc3c18a35188caa05d42b
unknown <khomanesuraj8@gmail.com>
2021-12-17 14:36:36 +0530

    Using put method Rest API call to display first name as paramter  and last name through query

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -55,4 +56,17 @@ public class HelloRestController {
     public String sayHello(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
     }
+    
+    /**
+     * @PutMapping:  mapping HTTP PUT requests onto specific handler methods.
+     * @param firstName
+     * @param lastName
+     * @return
+     */
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + " From bridgeLabz";
+    }
+
 }

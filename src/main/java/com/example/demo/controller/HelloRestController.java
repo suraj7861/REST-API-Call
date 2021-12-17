ef78181f30221d85d98e11ccf933e247dd918fcf
unknown <khomanesuraj8@gmail.com>
2021-12-17 11:32:27 +0530

    pass name as path variable and display message

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,5 +31,15 @@ public class HelloRestController {
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name + " From Bridgelabz";
 	}
+	
+	/**
+	 * @GetMapping: mapping HTTP GET requests onto specific handler methods
+	 * @param name
+	 * @return
+	 */
+    @GetMapping("/param/{name}")
+    public String sayHelloPara(@PathVariable String name) {
+        return "Hello " + name + " From Bridgelabz";
+    }
 
 }
